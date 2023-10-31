bbe5cad0735eb7d62cfe6f401c3764cbe1d4f297
Twaik Yont <twaikyont@gmail.com>
2023-10-31 21:04:35 +0200

    bump(main/python-torch): 2.1.0

--- packages/python-torch/build.sh
+++ packages/python-torch/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://pytorch.org/
 TERMUX_PKG_DESCRIPTION="Tensors and Dynamic neural networks in Python"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.0.1
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION="2.1.0"
 TERMUX_PKG_SRCURL=git+https://github.com/pytorch/pytorch
+TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 TERMUX_PKG_DEPENDS="ffmpeg, libc++, libopenblas, libprotobuf, libzmq, opencv, python, python-numpy, python-pip"
 TERMUX_PKG_BUILD_DEPENDS="vulkan-headers, vulkan-loader-android"
 TERMUX_PKG_HOSTBUILD=true
@@ -77,6 +77,7 @@ termux_step_pre_configure() {
 	"
 
 	ln -sf "$TERMUX_PKG_BUILDDIR" build
+	BAZEL_CXXOPTS="-std=c++20"
 }
 
 termux_step_make_install() {
